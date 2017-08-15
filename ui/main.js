33fb928ff79dd852255f9a28eced36ff31c1d36b
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-08-16 00:48:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ var element = document.getElementById('main-text'
     );
     
 element.innerHTML = 'New value';
+
+// Move the image
+var img = document.getElementById('img');
+img.onclick = function () {
+    img.style.marginleft = '100px';
+};
