42f8f5aec5e63d66f1b4c79e52ad7bf28a9dc62d
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-08-16 13:41:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,59 +1,45 @@
-// Counter code
+//Counter Code
 var button = document.getElementById('counter');
+var counter = 0;
 
-button.onclick = function () {
-            
-    // Create a request object
-    var  request = new XMLHttpRequest();
-    
-    // Capture the responce and store it in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if (request.status === 200) {
-                var counter = request.responseText; 
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        // Not done yet
-    };
-    
-    // Make the request
-    request.open('GET', 'http://patilvicky1212.imad.hasura-app.io/counter', true);
-    request.send(null);
-}; 
+button.onclick = function() {
 
-// Submit name
+//create a request to counter endpoint
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+if(request.readyState == XMLHttpRequest.DONE)
+{
+if(request.status==200)
+{
+var counter = request.responseText;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+}
+}
+//not yet done - no action reqd
+};
+
+//make a request
+request.open('GET','http://girijaiyer1996.imad.hasura-app.io/counter',true);
+request.send(null);
+
+};
+
+//submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    
-        // Create a request object
-    var  request = new XMLHttpRequest();
-    
-    // Capture the responce and store it in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if (request.status === 200) {
-                // Capture a list of names and render it as a list
-                var names = request.responceText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i< names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;                
-            }
-        }
-        // Not done yet
-    };
-    
-    // Make the request
-    request.open('GET', 'http://patilvicky1212.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+//make a request to the server
 
+//capture a list of names and render it as a list
+var names = ['name1','name2','name3'];
+var list = '';
+for(var i=0;i< names.length; i++)
+{
+    list +='<li>' + names[i] + '</li>';
+}
+ var ul = document.getElementById('namelist');
+ ul.innerHTML = list;
 };
