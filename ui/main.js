4fd0d32b1d52ab0feef0cbf27303743a6a5180a5
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-08-16 13:57:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,29 +31,17 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
         // Create a request object
-    var  request = new XMLHttpRequest();
     
     // Capture the responce and store it in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
+
             // Take some action
-            if (request.status === 200) {
+          
                 // Capture a list of names and render it as a list
-                var names = request.responceText;
-                names = JSON.parse(names);
+                var names = ['name1', 'name2', 'name3'];
                 var list = '';
                 for (var i=0; i< names.length; i++) {
                     list += '<li>' + names[i] + '</li>';
                 }
                 var ul = document.getElementById('namelist');
-                ul.innerHTML = list;                
-            }
-        }
-        // Not done yet
-    };
-    
-    // Make the request
-    request.open('GET', 'http://patilvicky1212.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
-
+                ul.innerHTML = list;
 };
