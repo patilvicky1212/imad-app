2a9da63e145ec691480163e726d2162e54ec8ccd
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-08-30 19:27:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,43 +13,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-
-var articles = {
-    'article-one': {
-    title: 'Article One | Amey Patil',
-    heading: 'Article One',
-    date: '14 Aug, 2017',
-    content: `
-        <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-        </p>
-        <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-        </p>
-        <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-        </p>`
-},
-    'article-two': {
-    title: 'Article Two | Amey Patil',
-    heading: 'Article Two',
-    date: '14 Aug, 2017',
-    content: `
-        <p>
-            This is the content for my second article. 
-        </p>`
-},
-    'article-three': {
-    title: 'Article Three | Amey Patil',
-    heading: 'Article Three',
-    date: '14 Aug, 2017',
-    content: `
-        <p>
-            This is the content for my third article. 
-        </p>`
-}
-};
-
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
