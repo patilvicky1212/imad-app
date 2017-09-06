15d99275607cf910cd1b2fb2452f88102077a802
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-09-06 18:26:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/create-user', function (req, res) {
            res.status(500).send(err.toString());
        } else {
            // res.send('User successfully created: ' + username);
-           res.send(JSON.parse('{"message":"User successfully created :"}') + username);
+           res.send(JSON.parse('{"message":"User successfully created: ' + username + ' "} ') );
        }           
     });
 });
