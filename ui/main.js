cbfd81034a9eb2df02ca2a34e360a960f16f0413
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-09-07 00:16:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,5 +29,5 @@ submit.onclick = function () {
     request.open('POST', 'http://patilvicky1212.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
-
+    
 };
