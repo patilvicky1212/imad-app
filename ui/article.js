5914ddd3159ec34e363163cf520ead5b33de6089
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-09-09 17:34:01 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,12 +3,12 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
     var commentFormHtml = `
-    <h5>Submit a comment</h5>
-    <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-    <br/>
-    <input type="submit" id="submit" value="Submit" />
-    <br/>
-    `;
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
@@ -19,24 +19,24 @@ function loadCommentForm () {
         
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
-            if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
                     document.getElementById('comment_text').value = '';
-                    loadComments();
+                    loadComments();    
                 } else {
                     alert('Error! Could not submit comment');
                 }
                 submit.value = 'Submit';
-            }
+          }
         };
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
-        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.open('POST', 'http://patilvicky1212.imad.hasura-app.io/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));
+        request.send(JSON.stringify({comment: comment}));  
         submit.value = 'Submitting...';
         
     };
@@ -44,7 +44,7 @@ function loadCommentForm () {
 
 function loadLogin () {
     // Check if the user is already logged in
-    var request = new XMLHttpTequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
@@ -53,11 +53,12 @@ function loadLogin () {
         }
     };
     
-    request.open('GET', '/check-login', true);
+    request.open('GET', 'http://patilvicky1212.imad.hasura-app.io/check-login', true);
     request.send(null);
 }
 
-function escapeHTML (text) {
+function escapeHTML (text)
+{
     var $text = document.createTextNode(text);
     var $div = document.createElement('div');
     $div.appendChild($text);
@@ -65,7 +66,7 @@ function escapeHTML (text) {
 }
 
 function loadComments () {
-    // Check if the user is already logged in
+        // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -73,7 +74,7 @@ function loadComments () {
             if (request.status === 200) {
                 var content = '';
                 var commentsData = JSON.parse(this.responseText);
-                for (var i = 0; i < commentsData.length; i++) {
+                for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
                     content += `<div class="comment">
                         <p>${escapeHTML(commentsData[i].comment)}</p>
@@ -89,11 +90,11 @@ function loadComments () {
         }
     };
     
-    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.open('GET', 'http://patilvicky1212.imad.hasura-app.io/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
 
 
-// The first thing to do is to check if the user is logged in
+// The first thing to do is to check if the user is logged in!
 loadLogin();
 loadComments();
