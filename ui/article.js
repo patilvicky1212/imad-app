f552b8d2402ae5dc7365fa6bd5d430fce411abf0
Amey Patil <30829786+patilvicky1212@users.noreply.github.com>
2017-09-09 17:24:37 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,12 +3,12 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
     var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
+    <h5>Submit a comment</h5>
+    <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+    <br/>
+    <input type="submit" id="submit" value="Submit" />
+    <br/>
+    `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
@@ -19,24 +19,24 @@ function loadCommentForm () {
         
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
                     document.getElementById('comment_text').value = '';
-                    loadComments();    
+                    loadComments();
                 } else {
                     alert('Error! Could not submit comment');
                 }
                 submit.value = 'Submit';
-          }
+            }
         };
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
+        request.send(JSON.stringify({comment: comment}));
         submit.value = 'Submitting...';
         
     };
